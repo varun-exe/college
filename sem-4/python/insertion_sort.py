2d80a317dc032e6c39a93c44b8a26fa6a2d239b7
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:39:28 +0530

    Update insertion_sort.py

--- sem-4/python/insertion_sort.py
+++ sem-4/python/insertion_sort.py
@@ -1,1 +1,15 @@
-
+num_list=[]
+n=int(input("enter number of elements: "))
+for i in range(n):
+    num=int(input("enter the elements: "))
+    num_list.append(num)
+for i in range(1,n):
+    picked=num_list[i]
+    j=i
+    while j>0 and picked<num_list[j-1]:
+        num_list[j]=num_list[j-1]
+        j-=1
+        num_list[j]=picked
+print("sorted array is:")
+for i in range(n):
+    print(num_list[i],end=" ")
