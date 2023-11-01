e4be4aba4d76fc7abf41f055ab5ae4228a78bb64
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:43:56 +0530

    Update word counting in a file.py

--- sem-4/python/word counting in a file.py
+++ sem-4/python/word counting in a file.py
@@ -1,1 +1,14 @@
+filename=input("enter the filename: ")
+file=open(filename,"r")
+n=int(input("enter number of lines to be displayed: "))
+for i in range(n):
+    line=file.readline();
+    print(line)
 
+file.seek(0)
+word=input("enter the word to be searched: ")
+count=0;
+for line in file:
+    if word in line:
+        count+=1
+print(f"the word {word} appears {count} times!")
