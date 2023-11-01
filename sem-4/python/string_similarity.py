47c0bfdce5f4660f1eabdfcf4a4cfc8c6cf8f57b
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:36:28 +0530

    Update string_similarity.py

--- sem-4/python/string_similarity.py
+++ sem-4/python/string_similarity.py
@@ -1,1 +1,9 @@
+import difflib
 
+def String_Similarity(str1,str2):
+    result = difflib.SequenceMatcher(a=str1.lower(),b = str2.lower())
+    return result.ratio()
+
+str1= input("Enter String 1:")
+str2= input("Enter String 2:")
+print(String_Similarity(str1,str2))
