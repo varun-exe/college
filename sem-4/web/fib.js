9b4f25a4549f931258442a558c7093fe455b4c20
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:57:28 +0530

    Update fib.js

--- sem-4/web/fib.js
+++ sem-4/web/fib.js
@@ -1,1 +1,25 @@
-
+var n = Number(prompt("Enter a number:"))
+var a = 0
+var b = 1
+var c = 0
+document.write("Fibonacci Series: "+"<br>")
+if(n==1){
+    document.write(a+"\n")
+}
+else if(n==2){
+    document.write(a+"<br>")
+    document.write(b+"<br>")
+}
+else if(n>2){
+    document.write(a+"<br>")
+    document.write(b+"<br>")
+    for(i=3;i<= n;i++){
+        c = a + b
+        a = b
+        b = c
+        document.write(c+"<br>")
+    }
+}
+else{
+    document.write("Wrong input")
+}
