c6cafdaad6ed88f393d26c3e79ddc0b91ebf412c
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:47:48 +0530

    Update palindrome_oops.py

--- sem-4/python/palindrome_oops.py
+++ sem-4/python/palindrome_oops.py
@@ -1,1 +1,26 @@
+class palindrome_checker:
+    def __init__(self,value):
+        self.value=value
+    def is_palindrome(self):
+        pass
 
+class strcheck(palindrome_checker):
+    def is_palindrome(self):
+        pal_str=''.join(self.value.split()).lower()
+        return pal_str==pal_str[::-1]
+
+class intcheck(palindrome_checker):
+    def is_palindrome(self):
+        int_str=str(self.value)
+        return int_str==int_str[::-1]
+
+a=input("enter string or integer")
+if a.isdigit():
+    res=intcheck(int(a))
+else:
+    res=strcheck(a)
+
+if res.is_palindrome():
+    print(f"{a} is a palindrome")
+else:
+    print(f"{a} is not a palindrome")
