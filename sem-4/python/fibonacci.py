896fb7f811d093194bd7b1a032afee6de5ea549f
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:34:22 +0530

    Update fibonacci.py

--- sem-4/python/fibonacci.py
+++ sem-4/python/fibonacci.py
@@ -1,1 +1,16 @@
+def fibo(x):
+    if x <= 0:
+        print("Invalid Input")
+    if x== 1:
+        return 0
+    elif x ==2:
+        return 1
+    else:
+        return fibo(x-2) + fibo(x-1)
+
+flag = "y"
+while(flag=="y"):
+    print(fibo(int(input("Enter number-"))))
+    flag=input("Do you want to continue?y/n-")
+    
 
