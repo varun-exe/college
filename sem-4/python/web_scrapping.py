f3886b7958bfbb0eb77109ba729a48918b6db75d
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:48:27 +0530

    Update web_scrapping.py

--- sem-4/python/web_scrapping.py
+++ sem-4/python/web_scrapping.py
@@ -1,1 +1,19 @@
+import os
+import requests
+from bs4 import BeautifulSoup
 
+os.makedirs('comics', exist_ok=True)
+for i in range(1, 10):
+    try:
+        page = requests.get(f"https://xkcd.com/{i}").content
+        soup = BeautifulSoup(page, "html.parser")
+        image = soup.find('div', {'id': "comic"}).img['src']
+
+        with open(os.path.join('comics', image.split("/")[-1]), "wb") as f:
+            f.write(requests.get("https://" + image[2:]).content)
+            print(f"Downloaded comic {i}")
+
+    except:
+        print(f"Could not download comic {i}")
+
+print("Done!")
