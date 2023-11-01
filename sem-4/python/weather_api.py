403078d3b8d45c7890b13ddf63c249329ccf98ff
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:51:12 +0530

    Update weather_api.py

--- sem-4/python/weather_api.py
+++ sem-4/python/weather_api.py
@@ -1,1 +1,23 @@
+#api-key=7bdccf825b7dd119a05ac2e0afaf1aaa
 
+import requests, json
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+CITY = input("Enter City name : ")
+API_KEY = input("Enter your API_KEY : ")
+URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+response = requests.get(URL)
+
+if response.status_code == 200:
+ data = response.json()
+ main = data['main']
+ temperature = main['temp']
+ humidity = main['humidity']
+ pressure = main['pressure']
+ report = data['weather']
+ print(f"{CITY:}")
+ print(f"Temperature: {temperature}")
+ print(f"Humidity: {humidity}")
+ print(f"Pressure: {pressure}")
+ print(f"Weather Report: {report[0]['description']}")
+else:
+ print("Error in the HTTP request")
