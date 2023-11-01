85756efc3780039ed568878c1b69a66c8fa9d3ba
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:46:20 +0530

    Update employee.py

--- sem-4/python/employee.py
+++ sem-4/python/employee.py
@@ -1,1 +1,36 @@
+import math
+
+class Shape:
+    def __init__(self):
+        self.name = ''
+        self.area = 0
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        self.name = "circle"
+
+    def calarea(self):
+        self.area = math.pi * self.radius * self.radius
+        print(f"Area is {self.area}")
+
+class Triangle(Shape):
+    def __init__(self, height, base):
+        self.height = height
+        self.base = base
+        self.name = "triangle"
+
+    def calarea(self):
+        self.area = 0.5 * self.base * self.height
+        print(f"Area is {self.area}")
+
+n1 = float(input("Enter radius: "))
+c = Circle(n1)
+c.calarea()
+
+h = float(input("Enter height: "))
+b = float(input("Enter base: "))
+
+t = Triangle(h, b)
+t.calarea()
 
