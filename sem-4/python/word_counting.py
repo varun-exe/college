85a7914643cdc3d28f0d7e1ea23bc7c9dc0f011a
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:35:45 +0530

    Update word_counting.py

--- sem-4/python/word_counting.py
+++ sem-4/python/word_counting.py
@@ -1,1 +1,17 @@
-
+sent = input("Enter a sentence:")
+numwords = sent.split()
+print("The number of words in the sentence is :",len(numwords))
+d = 0
+u = 0
+l = 0
+for char in sent:
+    if char.isdigit():
+        d += 1
+    elif char.isupper():
+        u += 1
+    elif char.islower():
+        l += 1
+    
+print(f"The number of digits in the sentence is:{d}")
+print(f"The number of Uppercase characters in the sentence is:{u}")
+print(f"The number of Lowercase characters in the sentence is:{l}")
