a95439c82928729b952e4ab3ec3f0e4f67e3e238
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:33:11 +0530

    Update palindrome.py

--- sem-4/python/palindrome.py
+++ sem-4/python/palindrome.py
@@ -1,1 +1,17 @@
+n = input("Enter a number-")
+rev = n[::-1]
+if rev == n:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
 
+freq = {}
+num = int(n)
+while num!=0:
+    digit = num%10
+    if digit in freq:
+        freq[digit] += 1
+    else:
+        freq[digit]=1
+    num = num//10
+print(freq)
