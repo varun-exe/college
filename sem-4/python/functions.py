4399a278f623afe01aab651ad11cfd37d46afd1a
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:35:02 +0530

    Update functions.py

--- sem-4/python/functions.py
+++ sem-4/python/functions.py
@@ -1,1 +1,21 @@
 
+def bintodec(n):
+    d = 0
+    p = 0 
+    while(n!=0):
+        rem = n %10
+        d = d + rem * (2**p)
+        p = p+ 1
+        n = n//10
+    return d
+print(bintodec(int(input("Enter a binary number-"))))
+
+def octtohex(n):
+    s = 0
+    c = 0
+    while n!=0:
+        s = s + (n%10)*8**c
+        n = n//10
+        c = c+1
+    print(hex(s))
+octtohex(int(input("Enter Octal Value-")))
