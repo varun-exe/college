c8cd1f1caf65dc0184bfef5cca8bbb67cb3115c7
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:31:50 +0530

    Update basic.py

--- sem-4/python/basic.py
+++ sem-4/python/basic.py
@@ -1,1 +1,9 @@
+test1 = int(input("Enter test 1 marks-"))
+test2 = int(input("Enter test 2 marks-"))
+test3 = int(input("Enter test 3 marks-"))
 
+total = test1 + test2 + test3
+
+avg = (total - min(test1,test2,test3))/2
+
+print("The best of two test average is ",avg)
