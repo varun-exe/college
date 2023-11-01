e03525cc2ee488554b6a16e2afdf0c0a4e78b3c9
varun-exe <92254766+varun-exe@users.noreply.github.com>
2023-11-01 17:41:18 +0530

    Update roman.py

--- sem-4/python/roman.py
+++ sem-4/python/roman.py
@@ -1,1 +1,14 @@
+def roman(s):
+    int_val = 0
+    for i in range(len(s)):
+        if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+            int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+        else:
+            int_val += rom_val[s[i]]
+    return int_val
 
+
+rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+rom=input(("enter string in roman:")).upper()
+int_val=roman(rom)
+print("integer is ",int_val)
